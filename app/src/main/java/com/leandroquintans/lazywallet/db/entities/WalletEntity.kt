a590542e9cbdadbbf8401bddc31072c7e338d2e5
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-03-27 20:33:04 +0000

    expanded Currency enum class

--- app/src/main/java/com/leandroquintans/lazywallet/db/entities/WalletEntity.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/db/entities/WalletEntity.kt
@@ -19,10 +19,79 @@ data class WalletEntity(
     //@ColumnInfo(name = "user_wallet")
     //val userWallet: Boolean = false
 ) {
-    enum class Currency {
-        AMERICAN_DOLLAR,
-        BRITISH_POUND,
-        EURO,
-        YEN,
+    enum class Currency(val verboseName: String, val format: String, val currencyValues: Array<String>) {
+        AMERICAN_DOLLAR(
+                "American Dollar ($)",
+                "%.2f",
+                arrayOf(
+                        "0.01",
+                        "0.05",
+                        "0.10",
+                        "0.25",
+                        "0.50",
+                        "1.00",
+                        "2.00",
+                        "5.00",
+                        "10.00",
+                        "20.00",
+                        "50.00",
+                        "100.00"
+                )
+        ),
+        BRITISH_POUND(
+                "British Pound (£)",
+                "%.2f",
+                arrayOf(
+                        "0.01",
+                        "0.02",
+                        "0.05",
+                        "0.10",
+                        "0.20",
+                        "0.50",
+                        "1.00",
+                        "2.00",
+                        "5.00",
+                        "10.00",
+                        "20.00",
+                        "50.00"
+                )
+        ),
+        EURO(
+                "Euro (€)",
+                "%.2f",
+                arrayOf(
+                        "0.01",
+                        "0.02",
+                        "0.05",
+                        "0.10",
+                        "0.20",
+                        "0.50",
+                        "1.00",
+                        "2.00",
+                        "5.00",
+                        "10.00",
+                        "20.00",
+                        "50.00",
+                        "100.00",
+                        "200.00",
+                        "500.00"
+                )
+        ),
+        YEN(
+                "Yen (¥)",
+                "%d",
+                arrayOf(
+                        "1",
+                        "5",
+                        "10",
+                        "50",
+                        "100",
+                        "500",
+                        "1000",
+                        "2000",
+                        "5000",
+                        "10000"
+                )
+        ),
     }
 }
