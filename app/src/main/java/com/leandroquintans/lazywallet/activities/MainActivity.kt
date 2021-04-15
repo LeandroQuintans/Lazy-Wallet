904be8822940180e585d25d5390adcf5aceddf8d
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-04-15 22:25:29 +0100

    data binding

---
+++ app/src/main/java/com/leandroquintans/lazywallet/activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.leandroquintans.lazywallet.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.leandroquintans.lazywallet.R
+import com.leandroquintans.lazywallet.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
