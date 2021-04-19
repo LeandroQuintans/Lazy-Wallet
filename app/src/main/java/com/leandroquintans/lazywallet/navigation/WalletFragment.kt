19857c69a9535725e6af8423a7d9993715ad4a33
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-04-19 01:41:49 +0100

    WalletFragment first version, needs viewmodel

--- app/src/main/java/com/leandroquintans/lazywallet/navigation/WalletFragment.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/navigation/WalletFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.leandroquintans.lazywallet.R
+import com.leandroquintans.lazywallet.databinding.FragmentWalletBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,8 +36,8 @@ class WalletFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false)
+        val binding = DataBindingUtil.inflate<FragmentWalletBinding>(inflater,
+            R.layout.fragment_wallet,container,false)
     }
 
     companion object {
