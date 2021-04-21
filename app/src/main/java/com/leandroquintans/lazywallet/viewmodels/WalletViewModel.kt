1355ba184619f4cf7b2820a3c7a73ec58f3e070b
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-04-22 00:37:22 +0100

    streamlined walletviewmodel

--- app/src/main/java/com/leandroquintans/lazywallet/viewmodels/WalletViewModel.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/viewmodels/WalletViewModel.kt
@@ -8,27 +8,43 @@ import com.leandroquintans.lazywallet.db.dao.WalletDao
 import com.leandroquintans.lazywallet.db.entities.WalletEntity
 import kotlinx.coroutines.launch
 
-class WalletViewModel(val database: WalletDao, application: Application) : AndroidViewModel(application) {
-    private var walletEntity = MutableLiveData<WalletEntity?>()
+class WalletViewModel(val database: WalletDao) : ViewModel() {
+    private var _walletEntity = MutableLiveData<WalletEntity?>()
+    val walletEntity: LiveData<WalletEntity?>
+        get() = _walletEntity
+
     val walletAmountString = Transformations.map(walletEntity) {
-        it?.currency?.formatWalletAmount(it?.wallet?.fullTotal)
+        it?.currency?.formatWalletAmount(it.wallet.fullTotal)
     }
 
     init {
-        Log.i("WalletViewModel", "WalletViewModel created!")
+        //Log.i("WalletViewModel", "WalletViewModel created!")
         initializeWallet()
     }
 
-    override fun onCleared() {
+    /*override fun onCleared() {
         super.onCleared()
         Log.i("WalletViewModel", "WalletViewModel destroyed!")
-    }
+    }*/
 
     private suspend fun getWalletFromDatabase(): WalletEntity? = database.getWallet()
+    /*private suspend fun deleteWalletFromDatabase() = _walletEntity.value?.let { database.deleteWallets(it) }
+    private suspend fun createWalletInDatabase(currency: WalletEntity.Currency) = database.insert(
+        WalletEntity(currency = currency)
+    )*/
 
     private fun initializeWallet() {
         viewModelScope.launch {
-            walletEntity.value = getWalletFromDatabase()
+            _walletEntity.value = getWalletFromDatabase()
         }
     }
+
+    /*fun changeCurrency(currency: WalletEntity.Currency) {
+        viewModelScope.launch {
+            deleteWalletFromDatabase()
+            createWalletInDatabase(currency)
+            //_walletEntity.value = getWalletFromDatabase()
+
+        }
+    }*/
 }
--- app/src/main/java/com/leandroquintans/lazywallet/viewmodels/WalletViewModelFactory.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/viewmodels/WalletViewModelFactory.kt
@@ -6,13 +6,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.leandroquintans.lazywallet.db.dao.WalletDao
 import kotlin.IllegalArgumentException
 
-class WalletViewModelFactory(
-    private val dataSource: WalletDao,
-    private val application: Application) : ViewModelProvider.Factory {
+class WalletViewModelFactory(private val dataSource: WalletDao) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(WalletViewModel::class.java)) {
-            return WalletViewModel(dataSource, application) as T
+            return WalletViewModel(dataSource) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
