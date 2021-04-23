390a50589971d545045c0d77d27585e27ab2854e
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-04-23 02:09:52 +0100

    WalletCoinUpdateFragment working

--- app/src/main/java/com/leandroquintans/lazywallet/navigation/WalletCoinUpdateFragment.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/navigation/WalletCoinUpdateFragment.kt
@@ -5,56 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.leandroquintans.lazywallet.R
+import com.leandroquintans.lazywallet.adapters.WalletCoinUpdateAdapter
+import com.leandroquintans.lazywallet.adapters.WalletCurrencyAdapter
+import com.leandroquintans.lazywallet.databinding.FragmentWalletCoinUpdateBinding
+import com.leandroquintans.lazywallet.databinding.FragmentWalletCurrencyChooseBinding
+import com.leandroquintans.lazywallet.db.AppDatabase
+import com.leandroquintans.lazywallet.viewmodels.WalletBaseViewModelFactory
+import com.leandroquintans.lazywallet.viewmodels.WalletCoinUpdateViewModel
+import com.leandroquintans.lazywallet.viewmodels.WalletCurrencyChooseViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WalletCoinUpdateFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WalletCoinUpdateFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentWalletCoinUpdateBinding
+    private lateinit var viewModel: WalletCoinUpdateViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet_coin_update, container, false)
-    }
+        binding = FragmentWalletCoinUpdateBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WalletCoinUpdateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WalletCoinUpdateFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val application = requireNotNull(this.activity).application
+        val dataSource = AppDatabase.getInstance(application).walletDao
+        val viewModelFactory = WalletBaseViewModelFactory(dataSource)
+
+        viewModel = ViewModelProvider(this, viewModelFactory).get(WalletCoinUpdateViewModel::class.java)
+
+        binding.lifecycleOwner = this
+
+        val adapter = WalletCoinUpdateAdapter(viewModel, viewLifecycleOwner)
+        binding.coinList.adapter = adapter
+
+        return binding.root
     }
 }
