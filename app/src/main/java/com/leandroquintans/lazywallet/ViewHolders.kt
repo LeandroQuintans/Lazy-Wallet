c033d3573b80c60cfb25c439d65beb51de215572
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-04-28 00:29:06 +0100

    deleted unused viewholder

--- app/src/main/java/com/leandroquintans/lazywallet/ViewHolders.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/ViewHolders.kt
@@ -1,16 +1,15 @@
 package com.leandroquintans.lazywallet
 
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
-
-class TextItemViewHolder(val textView: TextView): RecyclerView.ViewHolder(textView)
+import com.leandroquintans.lazywallet.databinding.CoinUpdateItemViewBinding
 
 class ButtonItemViewHolder(val button: Button): RecyclerView.ViewHolder(button)
 
-class CoinUpdateItemViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView) {
-    val textView: TextView = itemView.findViewById(R.id.coinValueText)
-    val editText: EditText = itemView.findViewById(R.id.coinAmountEditText)
+class CoinUpdateItemViewHolder(val binding: CoinUpdateItemViewBinding): RecyclerView.ViewHolder(binding.root) {
+    val textView: TextView = binding.coinValueText
+    val editText: EditText = binding.coinAmountEditText
 }
