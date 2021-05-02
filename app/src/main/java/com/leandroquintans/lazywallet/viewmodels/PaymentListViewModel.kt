1221846dfc2a35560b8441a06e72d09225235f07
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-05-02 18:14:35 +0100

    added LiveData for payment selection

--- app/src/main/java/com/leandroquintans/lazywallet/viewmodels/PaymentListViewModel.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/viewmodels/PaymentListViewModel.kt
@@ -1,6 +1,8 @@
 package com.leandroquintans.lazywallet.viewmodels
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import coincost.CoinCost
 import coincost.Wallet
@@ -12,7 +14,11 @@ import java.math.BigDecimal
 class PaymentListViewModel(database: WalletDao, private val cost: BigDecimal) : WalletBaseViewModel(database) {
     var payments = emptyList<Wallet>()
 
-    private suspend fun calculatePayments(): Set<Wallet> {
+    private val _selectedPayment = MutableLiveData<Int?>()
+    val selectedPayment: LiveData<Int?>
+        get() = _selectedPayment
+
+    private suspend fun calculatePayments(): List<Wallet> {
         with(Dispatchers.IO) {
             val coinCost = CoinCost(walletEntity.value?.wallet, cost)
             return coinCost.payments().toList()
@@ -23,5 +29,9 @@ class PaymentListViewModel(database: WalletDao, private val cost: BigDecimal) : 
         viewModelScope.launch {
             payments = calculatePayments()
         }
+    }
+
+    fun selectPayment(i: Int?) {
+        _selectedPayment.value = i
     }
 }
