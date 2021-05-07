eb9efb498e9df210c9874c0e60b82b2859881684
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-05-07 23:28:00 +0100

    added up button and changed fragment labels

--- app/src/main/java/com/leandroquintans/lazywallet/navigation/MainActivity.kt
+++ app/src/main/java/com/leandroquintans/lazywallet/navigation/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.leandroquintans.lazywallet.R
 import com.leandroquintans.lazywallet.databinding.ActivityMainBinding
 
@@ -14,6 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        this.findNavController(R.id.nav_host_fragment)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
